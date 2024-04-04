6047db8fdec5a441f0e35d5bbf5a7d635d583f4b
hunts <huntscorp@gmail.com>
2024-04-04 22:30:30 +0200

    Test CategoriesController ok

--- src/test/java/com/example/servicecatalogue/controllers/TestCategoriesController.java
+++ src/test/java/com/example/servicecatalogue/controllers/TestCategoriesController.java
@@ -1,33 +1,45 @@
 package com.example.servicecatalogue.controllers;
 
 import com.example.servicecatalogue.dtos.CategorieDTO;
+import com.example.servicecatalogue.dtos.pagination.PaginateRequestDTO;
+import com.example.servicecatalogue.exceptions.CategorieNotFoundException;
 import com.example.servicecatalogue.services.ServiceCategorie;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.doReturn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 class TestCategoriesController extends TestConfigurationControlleurRest {
 
     @MockBean
     private ServiceCategorie serviceCategorie;
 
+    @MockBean
+    private Validator validator;
+
+    /*========== CreerCategories ==========*/
+
     /**
      * Si non connecté, Unauthorized
      * @param mvc
-     * @param objectMapper
      * @throws Exception
      */
     @Test
-    void testCreerCategoriesUnauthorized(@Autowired MockMvc mvc, @Autowired ObjectMapper objectMapper) throws Exception {
+    void testCreerCategoriesUnauthorized(@Autowired MockMvc mvc) throws Exception {
         // BEFORE
 
         // WHERE
@@ -66,7 +78,7 @@ class TestCategoriesController extends TestConfigurationControlleurRest {
     }
 
     /**
-     * Si non admin, Forbidden
+     * Si admin, Created
      * @param mvc
      * @param objectMapper
      * @throws Exception
@@ -91,4 +103,295 @@ class TestCategoriesController extends TestConfigurationControlleurRest {
         // WHEN
         Assertions.assertEquals(HttpStatus.CREATED.value(), response.getStatus());
     }
+
+    /*========== GetAllCategories ==========*/
+
+    /**
+     * Si contrainte violation ok
+     * @param mvc
+     * @throws Exception
+     */
+    @Test
+    void testGetAllCategoriesOk(@Autowired MockMvc mvc) throws Exception {
+        // BEFORE
+        Set<ConstraintViolation<PaginateRequestDTO>> violations = new HashSet<>();
+        PaginateRequestDTO paginateRequest = new PaginateRequestDTO(0,10, null, Sort.Direction.ASC);
+        doReturn(violations).when(validator).validate(paginateRequest);
+
+        // WHERE
+        MockHttpServletResponse response = mvc.perform(
+                get("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // WHEN
+        Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    /**
+     * Si contrainte violation ok
+     * @param mvc
+     * @throws Exception
+     */
+    @Test
+    void testGetAllCategories(@Autowired MockMvc mvc) throws Exception {
+        // BEFORE
+        Set<ConstraintViolation<PaginateRequestDTO>> violations = new HashSet<>();
+        ConstraintViolation<PaginateRequestDTO> violation = null;
+        violations.add(violation);
+        PaginateRequestDTO paginateRequest = new PaginateRequestDTO(0,10, null, Sort.Direction.ASC);
+        doReturn(violations).when(validator).validate(paginateRequest);
+
+        // WHERE
+        MockHttpServletResponse response = mvc.perform(
+                get("/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // WHEN
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST.value(), response.getStatus());
+    }
+
+    /*========== GetCategoryDetails ==========*/
+
+    /**
+     * Categorie Not Found
+     * @param mvc
+     * @throws Exception
+     */
+    @Test
+    void  testgetCategoryDetailsNotFound(@Autowired MockMvc mvc) throws Exception {
+        //BEFORE
+        doThrow(CategorieNotFoundException.class).when(serviceCategorie).getCategoryById(2);
+
+        // WHERE
+        MockHttpServletResponse response = mvc.perform(
+                get("/categories/2")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        // WHEN
+        Assertions.assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatus());
+    }
+
+    /**
+     *  Categorie found : ok
+     * @param mvc
+     * @throws Exception
+     */
+    @Test
+    void  testgetCategoryDetailsOk(@Autowired MockMvc mvc) throws Exception {
+        //BEFORE
+        CategorieDTO categorieDTO = new CategorieDTO(1, "libelle", 2);
+        doReturn(categorieDTO).when(serviceCategorie).getCategoryById(1);
+
+        // WHERE
+        MockHttpServletResponse response = mvc.perform(
+                get("/categories/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //WHEN
+        Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    /*========== UpdateCategory ==========*/
+
+    /**
+     * Si admin et Categorie found : Ok
+     * @param mvc
+     * @param objectMapper
+     * @throws Exception
+     */
+    @Test
+    void testUpdateCategoryOk(@Autowired MockMvc mvc, @Autowired ObjectMapper objectMapper) throws Exception {
+        //BEFORE
+        CategorieDTO categorieDTO = new CategorieDTO(1, "libelle", 2);
+        // Définie l'admin en admin
+        this.defineAdminUser();
+        doReturn(categorieDTO).when(serviceCategorie).updateCategory(1, categorieDTO);
+
+        //WHERE
+        MockHttpServletResponse response = mvc.perform(
+                put("/categories/1")
+                        .header("Authorization", "Bearer "+getAccessToken())
+                        .content(objectMapper.writeValueAsString(categorieDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //WHEN
+        Assertions.assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    /**
+     * Si non connecte : Unauthorized
+     * @param mvc
+     * @param objectMapper
+     * @throws Exception
+     */
+    @Test
+    void testUpdateCategoryUnauthorized(@Autowired MockMvc mvc, @Autowired ObjectMapper objectMapper) throws Exception {
+        //BEFORE
+        CategorieDTO categorieDTO = new CategorieDTO(1, "libelle", 2);
+
+        //WHERE
+        MockHttpServletResponse response = mvc.perform(
+                put("/categories/1")
+                        .content(objectMapper.writeValueAsString(categorieDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //WHEN
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED.value(), response.getStatus());
+    }
+
+    /**
+     * Si non admin, Forbidden
+     * @param mvc
+     * @param objectMapper
+     * @throws Exception
+     */
+    @Test
+    void testUpdateCategoryNotAdminForbiden(@Autowired MockMvc mvc, @Autowired ObjectMapper objectMapper) throws Exception {
+        //BEFORE
+        CategorieDTO categorieDTO = new CategorieDTO(1, "libelle", 2);
+
+        //WHERE
+        MockHttpServletResponse response = mvc.perform(
+                put("/categories/1")
+                        .header("Authorization", "Bearer "+getAccessToken())
+                        .content(objectMapper.writeValueAsString(categorieDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //WHEN
+        Assertions.assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
+    }
+
+    /**
+     * Si Categorie not found
+     * @param mvc
+     * @param objectMapper
+     * @throws Exception
+     */
+    @Test
+    void testUpdateCategoryCategoryNotFound(@Autowired MockMvc mvc, @Autowired ObjectMapper objectMapper) throws Exception {
+        //BEFORE
+        CategorieDTO categorieDTO = new CategorieDTO(1, "libelle", 2);
+        // Définie l'admin en admin
+        this.defineAdminUser();
+        doThrow(CategorieNotFoundException.class).when(serviceCategorie).updateCategory(1, categorieDTO);
+
+        //WHERE
+        MockHttpServletResponse response = mvc.perform(
+                put("/categories/2")
+                        .header("Authorization", "Bearer "+getAccessToken())
+                        .content(objectMapper.writeValueAsString(categorieDTO))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //WHEN
+        Assertions.assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatus());
+    }
+
+    /*========== deleteCategory ==========*/
+
+    /**
+     * Si admin et Categorie found : Not_Content
+     * @param mvc
+     * @throws Exception
+     */
+    @Test
+    void testDeleteCategoryOk(@Autowired MockMvc mvc) throws Exception {
+        //BEFORE
+        CategorieDTO categorieDTO = new CategorieDTO(1, "libelle", 2);
+        // Définie l'admin en admin
+        this.defineAdminUser();
+        doNothing().when(serviceCategorie).deleteCategory(1);
+
+        //WHERE
+        MockHttpServletResponse response = mvc.perform(
+                delete("/categories/1")
+                        .header("Authorization", "Bearer "+getAccessToken())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //WHEN
+        Assertions.assertEquals(HttpStatus.NO_CONTENT.value(), response.getStatus());
+    }
+
+    /**
+     * Si admin et Categorie not found : Not found
+     * @param mvc
+     * @throws Exception
+     */
+    @Test
+    void testDeleteCategoryNotFound(@Autowired MockMvc mvc) throws Exception {
+        //BEFORE
+        CategorieDTO categorieDTO = new CategorieDTO(1, "libelle", 2);
+        // Définie l'admin en admin
+        this.defineAdminUser();
+        doThrow(CategorieNotFoundException.class).when(serviceCategorie).deleteCategory(2);
+
+        //WHERE
+        MockHttpServletResponse response = mvc.perform(
+                delete("/categories/2")
+                        .header("Authorization", "Bearer "+getAccessToken())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //WHEN
+        Assertions.assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatus());
+    }
+
+    /**
+     * Si non connecte : Unauthorized
+     * @param mvc
+     * @throws Exception
+     */
+    @Test
+    void testDeleteCategoryUnothorized(@Autowired MockMvc mvc) throws Exception {
+
+        //WHERE
+        MockHttpServletResponse response = mvc.perform(
+                delete("/categories/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //WHEN
+        Assertions.assertEquals(HttpStatus.UNAUTHORIZED.value(), response.getStatus());
+    }
+
+    /**
+     * Si non admin : Forbidden
+     * @param mvc
+     * @throws Exception
+     */
+    @Test
+    void testDeleteCategoryForbidden(@Autowired MockMvc mvc) throws Exception {
+
+        //WHERE
+        MockHttpServletResponse response = mvc.perform(
+                delete("/categories/1")
+                        .header("Authorization", "Bearer "+getAccessToken())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+
+        //WHEN
+        Assertions.assertEquals(HttpStatus.FORBIDDEN.value(), response.getStatus());
+    }
+
+
 }
